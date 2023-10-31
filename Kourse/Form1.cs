d42e18a9acd90fc3867b2603ffb3c01062ec617a
GrafTumbochkin <134737016+GrafTumbochkin@users.noreply.github.com>
2023-10-31 09:43:54 +0300

    Update Form1.cs
    Добавление библиотек и попытка подключения к бд

--- Kourse/Form1.cs
+++ Kourse/Form1.cs
@@ -1,10 +1,24 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+//добавление библиотек
 namespace Kourse
 {
     public partial class Form1 : Form
     {
+    //первая попытка подключения к бд
+    string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CompanyDatabase;Integrated Security=True";
+    SqlConnection connection;
+    SqlDataAdapter adapter;
+    DataSet dataSet;
         public Form1()
         {
             InitializeComponent();
+            connection = new SqlConnection(connectionString);
+            adapter = new SqlDataAdapter();
+            InitializeDataSet();
+            LoadData();
         }
 
         private void Form1_Load(object sender, EventArgs e)
