3b8d9e902edd0aa8ec1e0499fddc2933b894741d
GrafTumbochkin <134737016+GrafTumbochkin@users.noreply.github.com>
2023-10-31 09:49:40 +0300

    Update3 Form1.cs
    Добавление таблиц Store  и Product

--- Kourse/Form1.cs
+++ Kourse/Form1.cs
@@ -28,16 +28,43 @@ namespace Kourse
         private void InitializeDataSet()
 {
     dataSet = new DataSet();
-//добавление параметров для таблицы Фирма
+//добавление параметров для таблицы Firm
     DataTable firmTable = new DataTable("Firm");
     firmTable.Columns.Add("ID", typeof(int));
     firmTable.Columns.Add("Name", typeof(string));
     firmTable.Columns.Add("Phone", typeof(string));
     firmTable.Columns.Add("Address", typeof(string));
 
-    
-   
+    //добавление параметров для таблицы Store
+    DataTable storeTable = new DataTable("Store");
+    storeTable.Columns.Add("ID", typeof(int));
+    storeTable.Columns.Add("Name", typeof(string));
+    storeTable.Columns.Add("Phone", typeof(string));
+    storeTable.Columns.Add("Address", typeof(string));
 
+//добавление параметров для таблицы Product
+    DataTable productTable = new DataTable("Product");
+    productTable.Columns.Add("ID", typeof(int));
+    productTable.Columns.Add("Name", typeof(string));
+    productTable.Columns.Add("Category", typeof(string));
+    productTable.Columns.Add("Description", typeof(string));
+    productTable.Columns.Add("Price", typeof(decimal));
+    productTable.Columns.Add("Quantity", typeof(int));
+    productTable.Columns.Add("Unit", typeof(string));
+    productTable.Columns.Add("ArrivalDate", typeof(DateTime));
+    productTable.Columns.Add("StoreID", typeof(int));
+    productTable.Columns.Add("Discount", typeof(decimal));
+    productTable.Columns.Add("VAT", typeof(decimal));
+
+    dataSet.Tables.Add(firmTable);
+    dataSet.Tables.Add(storeTable);
+    dataSet.Tables.Add(productTable);
+
+    DataRelation storeFirmRelation = new DataRelation("FK_Store_Firm",dataSet.Tables["Firm"].Columns["ID"],dataSet.Tables["Store"].Columns["ID"]);
+    DataRelation productStoreRelation = new DataRelation("FK_Product_Store",dataSet.Tables["Store"].Columns["ID"], dataSet.Tables["Product"].Columns["StoreID"]);
+
+
+    
 }
     }
 }
